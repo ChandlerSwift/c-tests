7acd341aea14a07f6c7cdb5492b91841069e309f
Chandler Swift <chandler+pearson@chandlerswift.com>
2020-02-12 12:34:18 -0600

    Add fibio

---
+++ fibio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "fib.h"
+
+int main() {
+	char buf[64];
+	int num;
+	while (1) {
+		printf("fibcalc> ");
+		fgets(buf, 64, stdin);
+		if (strncmp(buf, "exit", 64) == 0) {
+			return 0;
+		} else if (num = strtol(buf, NULL, 10) && errno != ERANGE) {
+			num = strtol(buf, NULL, 10);
+			printf("Fibonacci number %d is %d.\n", num, fib(num));
+		} else {
+			fprintf(stderr, "An error occurred.");
+			return 1;
+		}
+	}
+}
