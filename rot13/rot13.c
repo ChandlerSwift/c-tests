a1aed3e2e36849fa7f33a89638234154f896b51c
Chandler Swift <chandler+pearson@chandlerswift.com>
2020-02-17 15:14:18 -0600

    Add rot13 program

---
+++ rot13/rot13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+	char ch;
+	while (read(STDIN_FILENO, &ch, 1) > 0) {
+		if ((ch >= 'A' && ch <= 'M') || (ch >= 'a' && ch <= 'm')) {
+			printf("%c", ch+13);
+		} else if ((ch >= 'N' && ch <= 'Z') || (ch >= 'n' && ch <= 'z')) {
+			printf("%c", ch-13);
+		} else {
+			printf("%c", ch);
+		}
+	}
+}
