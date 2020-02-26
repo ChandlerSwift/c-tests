c33f2e1a4d31a1aad280e824083bd4e35cb5ed75
Chandler Swift <chandler@chandlerswift.com>
2020-02-25 22:11:43 -0600

    Add banner to rot13

--- rot13/rot13.c
+++ rot13/rot13.c
@@ -3,6 +3,7 @@
 
 int main() {
 	char ch;
+	printf("CHANDLER'S ROT13 [EN|DE]CODER\n");
 	while (read(STDIN_FILENO, &ch, 1) > 0) {
 		if ((ch >= 'A' && ch <= 'M') || (ch >= 'a' && ch <= 'm')) {
 			printf("%c", ch+13);
