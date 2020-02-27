0f6785eee2b6e63b67f3cc5f112f9d69472b6e6a
Chandler Swift <chandler+pearson@chandlerswift.com>
2020-02-27 13:55:36 -0600

    Convert spaces to tabs

--- rot13/asm/rot13.s
+++ rot13/asm/rot13.s
@@ -22,18 +22,18 @@ top:
 	cmp $4, %eax
 	je exit
 
-        # write(1, buffer, 1)
-        mov     $1, %rax                # system call 1 is write
-        mov     $1, %rdi                # file handle 1 is stdout
-        mov     $buffer, %rsi           # address of string to output
-        mov     $1, %rdx                # number of bytes
-        syscall                         # invoke operating system to do the write
+	# write(1, buffer, 1)
+	mov     $1, %rax                # system call 1 is write
+	mov     $1, %rdi                # file handle 1 is stdout
+	mov     $buffer, %rsi           # address of string to output
+	mov     $1, %rdx                # number of bytes
+	syscall                         # invoke operating system to do the write
 	jmp top
 exit:
-        # exit(0)
-        mov     $60, %rax               # system call 60 is exit
-        xor     %rdi, %rdi              # we want return code 0
-        syscall                         # invoke operating system to exit
+	# exit(0)
+	mov     $60, %rax               # system call 60 is exit
+	xor     %rdi, %rdi              # we want return code 0
+	syscall                         # invoke operating system to exit
 .data
 buffer:
 	.space 1
