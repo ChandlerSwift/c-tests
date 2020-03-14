234184ce912f024bac839122112bacb0fb41eec6
Chandler Swift <chandler@chandlerswift.com>
2020-03-13 19:28:11 -0500

    Add rot13 help flag

--- rot13/rot13.c
+++ rot13/rot13.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char** argv) {
+	if (argc > 1) {
+		printf("Usage: simply pipe text to stdin. For example:\n\n  cat text | ./rot13\n");
+		return 1;
+	}
 	char ch;
 	while (read(STDIN_FILENO, &ch, 1) > 0) {
 		if ((ch >= 'A' && ch <= 'M') || (ch >= 'a' && ch <= 'm')) {
